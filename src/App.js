fce197695fd45b622a23a06d30886045454125a6
Saud <saudtauqeer381@gmail.com>
2020-05-20 04:12:02 +0500

    added code for appending chungus.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
-
+import { Frame, Scroll, useCycle } from "framer";
 import "./index.css";
 
 function App() {
-  const [isDefault, setIsDefault] = useState(false);
-  if (!isDefault) {
-    return "false";
+  const [count, setCount] = useState(1);
+  function handleOnClick() {
+    setCount((prev) => setCount(prev + 1));
   }
-  if (isDefault) {
-    return "true";
-  }
-  return null;
-}
 
-function DefaultChungus() {}
+  function Chungus() {
+    return (
+      <Frame
+        width="200px"
+        height="200px"
+        animate={{ rotate: 360 }}
+        transition={{ duration: 1 }}
+      >
+        {Array(count).fill(
+          <img
+            style={{ display: "block" }}
+            onClick={handleOnClick}
+            width="200px"
+            height="200px"
+            src="chungus.jpg"
+          />
+        )}
+      </Frame>
+    );
+  }
+  return <Chungus />;
+}
 
 export default App;
