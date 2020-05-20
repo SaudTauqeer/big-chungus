4331eb23f15734142f7ebcc6035112e2b2dd3b57
Saud <saudtauqeer381@gmail.com>
2020-05-20 16:24:46 +0500

    fixed typo import.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import LazyLoad from "react-lazyload";
 import Chungus from "./Components/Chungus";
 import "./index.css";
 
