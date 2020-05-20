f647b742c85cfc16ebeeb49d745e9a2d85fb132b
Saud <saudtauqeer381@gmail.com>
2020-05-20 16:23:50 +0500

    removed lazy loading dep. added loading indicator for image.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import LazyLoad from "react-lazyload";
 import Chungus from "./Components/Chungus";
 import "./index.css";
 
@@ -9,6 +10,7 @@ const events = ["click", "resize"];
 function App() {
   const [vw, setVw] = useState(document.documentElement.clientWidth);
   const [vh, setVh] = useState(document.documentElement.clientHeight);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   function clickHandler(e) {
     const currentElClass = e.target.className;
@@ -45,7 +47,27 @@ function App() {
 
   return (
     <>
-      <Chungus vh={vh} vw={vw} count={count} />
+      <Chungus
+        setImageLoaded={setImageLoaded}
+        imageLoaded={imageLoaded}
+        vh={vh}
+        vw={vw}
+        count={count}
+      />
+      {!imageLoaded ? (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+            justifyContent: "center",
+          }}
+        >
+          <p style={{ textAlign: "center" }}>Loading big chungus</p>
+        </div>
+      ) : null}
       <div style={{ position: "absolute", bottom: 0, zIndex: "999999" }}>
         <p
           className="no-event"
--- src/Components/Chungus.js
+++ src/Components/Chungus.js
@@ -1,10 +1,10 @@
 import { motion } from "framer-motion";
-import LazyLoad from "react-lazyload";
-import React from "react";
+
+import React, { useState } from "react";
 //util
 import { getRandomInt } from "../util/index";
 
-function Chungus({ count, vw, vh }) {
+function Chungus({ count, vw, vh, setImageLoaded, imageLoaded }) {
   console.log("chungus");
   return (
     <div style={{ display: "flex", flexWrap: "wrap" }}>
@@ -12,26 +12,25 @@ function Chungus({ count, vw, vh }) {
         .fill()
         .map((e, i) => {
           return (
-            <LazyLoad height={200}>
-              <motion.img
-                className="no-event"
-                transition={{
-                  duration: 4,
-                  loop: Infinity,
-                  ease: "linear",
-                }}
-                drag
-                animate={{
-                  x: getRandomInt(0, vw),
-                  y: getRandomInt(0, vh),
-                }}
-                key={i}
-                style={{ display: "block" }}
-                width="200px"
-                height="200px"
-                src="chungus.jpg"
-              />
-            </LazyLoad>
+            <motion.img
+              onLoad={() => setImageLoaded(true)}
+              className="no-event"
+              transition={{
+                duration: 4,
+                loop: Infinity,
+                ease: "linear",
+              }}
+              drag
+              animate={{
+                x: getRandomInt(0, vw),
+                y: getRandomInt(0, vh),
+              }}
+              key={i}
+              style={imageLoaded ? { display: "block" } : { display: "none" }}
+              width="200px"
+              height="200px"
+              src="chungus.jpg"
+            />
           );
         })}
     </div>
